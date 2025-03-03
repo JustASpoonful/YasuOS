5b96a3a58722a5df3e8bbf1614d43e88f15da46b
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2025-03-03 13:24:17 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -55,13 +55,5 @@ const appList = [
     installed: false,
     preinstalled: false,
     description: "Unlock advanced features."
-  },
-  {
-    name: "PornHub",
-    icon: "https://static.vecteezy.com/system/resources/thumbnails/020/111/478/small_2x/pro-letter-logo-design-on-black-background-pro-creative-circle-logo-pro-initials-letter-logo-concept-pro-letter-design-vector.jpg",
-    content: "app/pornhub.html",
-    installed: false,
-    preinstalled: false,
-    description: "Fill your sexual desires"
-  },
+  }
 ];
